c1f5b16893af9123d14cc3b207fb4dc01fa17b0f
liuj-ai <liuj-ai@glodon.com>
2019-04-17 15:16:57 +0800

    序列化模块增加测试代码

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # via project
     Via is a middleware project.
 
-## via light rpc
+## via sf
     This is a fast service framework.
---
+++ via-sf/via-sf-serialization/src/test/java/com/via/msgpack/UserInfo.java
@@ -0,0 +1,33 @@
+package com.via.msgpack;
+
+import org.msgpack.BeanMessage;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by stereo on 16-8-5.
+ */
+public class UserInfo implements BeanMessage {
+
+    public class A{}
+
+    public long s = 100;
+
+    public King king = new King();
+
+    public Object object = new String("hellow");
+
+    public RuntimeException ipcRuntimeException = new RuntimeException("test",new RuntimeException("不对"));
+
+    public static void main(String[] args) {
+        Method cls = UserInfo.class.getMethods()[0];
+        System.out.println(cls.getDeclaringClass());
+    }
+
+    public class InnerClass1 {
+        public InnerClass1() {
+            System.out.println("Inner Class1");
+        }
+    }
+
+}
